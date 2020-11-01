9dca8f052a0c1bccf764178f16732aef60788d99
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-01 04:26:30 +0300

    react-router-hash-link applied

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './index.css';
 import { Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const Navigation = () => {
     return (
@@ -8,21 +10,23 @@ const Navigation = () => {
             <Navbar.Toggle aria-controls='responsive-navbar-nav' />
             <Navbar.Collapse id='responsive-navbar-nav'>
                 <Nav className='navs d-flex justify-content-around'>
-                    <Nav.Link href='#home' className='text-light'>
-                        Home
-                    </Nav.Link>
-                    <Nav.Link href='#about' className='text-light'>
-                        About
-                    </Nav.Link>
-                    <Nav.Link href='#resume' className='text-light'>
-                        Resume
-                    </Nav.Link>
-                    <Nav.Link href='#projects' className='text-light'>
-                        Projects
-                    </Nav.Link>
-                    <Nav.Link href='#contact' className='text-light'>
-                        Contact
-                    </Nav.Link>
+                    <Router>
+                        <HashLink smooth to='#top' className='text-light'>
+                            Home
+                        </HashLink>
+                        <HashLink smooth to='#about' className='text-light'>
+                            About
+                        </HashLink>
+                        <HashLink smooth to='#resume' className='text-light'>
+                            Resume
+                        </HashLink>
+                        <HashLink smooth to='#projects' className='text-light'>
+                            Projects
+                        </HashLink>
+                        <HashLink smooth to='#contact' className='text-light'>
+                            Contact
+                        </HashLink>
+                    </Router>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,6 +4,9 @@ import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/Sidebar';
 import Introduction from '../../components/Introduction';
 import About from '../../components/About';
+import Contact from '../../components/Contact';
+import Projects from '../../components/Projects';
+import Resume from '../../components/Resume';
 
 const Home = () => {
     return (
@@ -12,6 +15,9 @@ const Home = () => {
             <Sidebar />
             <Introduction />
             <About />
+            <Projects />
+            <Resume />
+            <Contact />
         </>
     );
 };
