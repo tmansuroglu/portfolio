a18347c3952b969a294e6df556ddca3fba660d26
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-29 03:38:30 +0300

    delete resume iframe

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -7,19 +7,16 @@ const Resume = () => {
         <Container>
             <h1 className='homeTitle'>Resume</h1>
             <a
-                href='https://docs.google.com/document/d/1ZJmw9fsLZJDZKE32afpMMZXos12eTN-c29GJtLfOp1Q/edit?usp=sharing'
+                href='https://drive.google.com/file/d/1hYvgvIL-4xm-cn7JKFCq-CIULs9LJx8E/view?usp=sharing'
                 target='_blank'
                 rel='noreferrer'
-                className='googleDocsLink'
+                className='hrefLink'
             >
-                View resume in Google Docs
+                Download resume PDF
             </a>
-            <iframe
-                src='resume.pdf'
-                title='resume'
-                id='resume'
-                className='resume'
-            />
+            <div id='resume' className='homeText'>
+                resume here
+            </div>
         </Container>
     );
 };
