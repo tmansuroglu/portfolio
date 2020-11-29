d6e5e7c8c1cff20c0622afd6ccb65faaaf9397fc
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-29 03:46:39 +0300

    add link and iframe

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -7,16 +7,29 @@ const Resume = () => {
         <Container>
             <h1 className='homeTitle'>Resume</h1>
             <a
+                href='https://docs.google.com/document/d/1ZJmw9fsLZJDZKE32afpMMZXos12eTN-c29GJtLfOp1Q/edit?usp=sharing'
+                target='_blank'
+                rel='noreferrer'
+                className='hrefLink'
+            >
+                View resume in Google Docs
+            </a>
+            <br />
+            <a
                 href='https://drive.google.com/file/d/1hYvgvIL-4xm-cn7JKFCq-CIULs9LJx8E/view?usp=sharing'
                 target='_blank'
                 rel='noreferrer'
                 className='hrefLink'
             >
-                Download resume PDF
+                Download resume from Google Drive
             </a>
-            <div id='resume' className='homeText'>
-                resume here
-            </div>
+            <br />
+            <iframe
+                src='resume.pdf'
+                title='resume'
+                id='resume'
+                className='resume'
+            />
         </Container>
     );
 };
