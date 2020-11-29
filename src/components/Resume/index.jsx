59c917bccf0b1178de7e3699a311c64e2143f7c2
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-30 00:54:04 +0300

    make button row responsive

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './index.css';
-import { Container, Button, Collapse } from 'react-bootstrap';
+import { Container, Button, Collapse, Row, Col } from 'react-bootstrap';
 
 const Resume = () => {
     const [isCollapsableOpen, setIsCollapsableOpen] = useState(false);
@@ -9,32 +9,42 @@ const Resume = () => {
             <h1 className='homeTitle' id='resume'>
                 Resume
             </h1>
-            <a
-                href='https://docs.google.com/document/d/1ZJmw9fsLZJDZKE32afpMMZXos12eTN-c29GJtLfOp1Q/edit?usp=sharing'
-                target='_blank'
-                rel='noreferrer'
-            >
-                <Button variant='light'>View resume in Google Docs</Button>
-            </a>
-            <Button
-                className='viewPdf'
-                variant='light'
-                onClick={() => setIsCollapsableOpen(!isCollapsableOpen)}
-                aria-controls='resumePdfCollapsable'
-                aria-expanded={isCollapsableOpen}
-            >
-                View Resume as PDF
-            </Button>
-            <a
-                href='https://drive.google.com/file/d/1hYvgvIL-4xm-cn7JKFCq-CIULs9LJx8E/view?usp=sharing'
-                target='_blank'
-                rel='noreferrer'
-                className='downloadResumePdf'
-            >
-                <Button variant='light'>
-                    Download Resume PDF from Google Drive
-                </Button>
-            </a>
+            <Row>
+                <Col xs={12} md={3} className='resumeButtonCol'>
+                    <a
+                        href='https://docs.google.com/document/d/1ZJmw9fsLZJDZKE32afpMMZXos12eTN-c29GJtLfOp1Q/edit?usp=sharing'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <Button variant='light' className='resumeButtonColItem'>
+                            View resume in Google Docs
+                        </Button>
+                    </a>
+                </Col>
+                <Col xs={12} md={3}>
+                    <Button
+                        variant='light'
+                        onClick={() => setIsCollapsableOpen(!isCollapsableOpen)}
+                        aria-controls='resumePdfCollapsable'
+                        aria-expanded={isCollapsableOpen}
+                        className='resumeButtonColItem viewPdf'
+                    >
+                        View Resume as PDF
+                    </Button>
+                </Col>
+                <Col xs={12} md={3}>
+                    <a
+                        href='https://drive.google.com/file/d/1hYvgvIL-4xm-cn7JKFCq-CIULs9LJx8E/view?usp=sharing'
+                        target='_blank'
+                        rel='noreferrer'
+                        className='downloadResumePdf'
+                    >
+                        <Button variant='light' className='resumeButtonColItem'>
+                            Download Resume From Google Drive
+                        </Button>
+                    </a>
+                </Col>
+            </Row>
             <Collapse in={isCollapsableOpen} id='resumePdfCollapsable'>
                 <div>
                     <iframe
