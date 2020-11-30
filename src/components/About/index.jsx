d5c4ecc64614ab5951284e8063de5328c5cb17f3
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-30 08:10:04 +0300

    fix about text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -11,19 +11,24 @@ const About = () => {
                     Tarkan Mansuroğlu is a front-end web developer. He started
                     learning coding by himself and continued his coding
                     education through “Re-Coded Front-End Web Development
-                    Bootcamp”. Tarkan Mansuroğlu is a passionate learner and
-                    enjoys complex problems.
+                    Bootcamp”. He became a coder because he is a lifelong
+                    learner and addicted to the sensation that comes after
+                    solving a tech problem.
                 </p>
                 <p>
-                    His desire to tackle difficult things always made him stand
-                    out. At age 20 he was severely obese and academically lost.
-                    At 22 he dropped from 175kg to 95 kg, became a fitness
+                    He discovered his appetite for challenges in his early 20s.
+                    At age 20, he was severely obese and academically lost. At
+                    22 he dropped from 175kg to 95 kg, became a fitness
                     enthusiast and top finance student of his university. Later,
                     he graduated with highest CGPA and continued being a
-                    physically active person. During these years he wanted to
-                    figure out what he expected from life. While trying, he
-                    developed an analytical mindset. Consequently, he got
-                    attracted to coding.
+                    physically active person.
+                </p>
+                <p>
+                    During these years, he wanted to figure out what he expected
+                    from life. While searching for his purpose, he realized that
+                    to build his future, first he needs to decypher himself.
+                    This process helped him develop an analytical mindset.
+                    Consequently, his new mentality lead him to coding.
                 </p>
                 <p>
                     He is currently looking for new challenges to conquer and
