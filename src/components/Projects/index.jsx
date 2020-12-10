6c34737684d2e7a418ec5a542032dd3d98bb932e
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-12-10 17:55:52 +0300

    fix order of projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -12,10 +12,10 @@ const Projects = () => {
       <h1 className='homeTitle' id='projects'>
         Projects
       </h1>
-      <VerticalTimeline animate={false} className='verticalTimeline'>
+      <VerticalTimeline className='verticalTimeline'>
         <VerticalTimelineElement
           className='vertical-timeline-element--work'
-          date={'Oct 2020'}
+          date={'Nov 2020'}
           dateClassName={'date'}
           contentStyle={{
             background: 'white',
@@ -81,6 +81,61 @@ const Projects = () => {
                   <li>Scrum master for 1 week</li>
                   <li>Helping colleagues whenever needed</li>
                   <li>Developing twitter section of homepage</li>
+                </ul>
+              </Col>
+            </Row>
+          </div>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          date={'Nov 2020'}
+          dateClassName={'date'}
+          className='vertical-timeline-element--work'
+          contentStyle={{
+            background: 'white',
+            color: 'white',
+          }}
+          contentArrowStyle={{
+            borderRight: '7px solid  white',
+          }}
+          iconStyle={{
+            background: 'white',
+          }}
+          icon={<i className='fas fa-project-diagram projectIcon'></i>}
+        >
+          <div className='homeText projectContent'>
+            <h4>Portfolio</h4>
+            <a
+              href='https://github.com/tmansuroglu/portfolio'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+            <br />
+            <p>
+              <strong>Goal:</strong> building a portfolio website to create an
+              online presence.
+            </p>
+            <br />
+            <Row>
+              <Col xs={12} md={6}>
+                <h5>Skills Applied</h5>
+                <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>Emailjs</li>
+                  <li>Bootstrap</li>
+                  <li>Responsive Design</li>
+                  <li>Git and Github</li>
+                  <li>Testing (Jest)</li>
+                </ul>
+              </Col>
+              <Col xs={12} md={6}>
+                <h5>My Responsibilities</h5>
+                <ul>
+                  <li>Solo Project</li>
                 </ul>
               </Col>
             </Row>
@@ -156,61 +211,6 @@ const Projects = () => {
                   <li>Contact page</li>
                   <li>Login page</li>
                   <li>Register page</li>
-                </ul>
-              </Col>
-            </Row>
-          </div>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          date={'Nov 2020'}
-          dateClassName={'date'}
-          className='vertical-timeline-element--work'
-          contentStyle={{
-            background: 'white',
-            color: 'white',
-          }}
-          contentArrowStyle={{
-            borderRight: '7px solid  white',
-          }}
-          iconStyle={{
-            background: 'white',
-          }}
-          icon={<i className='fas fa-project-diagram projectIcon'></i>}
-        >
-          <div className='homeText projectContent'>
-            <h4>Portfolio</h4>
-            <a
-              href='https://github.com/tmansuroglu/portfolio'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-            <br />
-            <p>
-              <strong>Goal:</strong> building a portfolio website to create an
-              online presence.
-            </p>
-            <br />
-            <Row>
-              <Col xs={12} md={6}>
-                <h5>Skills Applied</h5>
-                <ul>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>JavaScript</li>
-                  <li>React</li>
-                  <li>Emailjs</li>
-                  <li>Bootstrap</li>
-                  <li>Responsive Design</li>
-                  <li>Git and Github</li>
-                  <li>Testing (Jest)</li>
-                </ul>
-              </Col>
-              <Col xs={12} md={6}>
-                <h5>My Responsibilities</h5>
-                <ul>
-                  <li>Solo Project</li>
                 </ul>
               </Col>
             </Row>
