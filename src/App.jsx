4d56fced0154d909dd0cdca1df342109abe2064d
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-12-11 23:44:49 +0300

    delete unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import Home from './containers/Home/index.jsx';
-import { Router } from 'react-router-dom';
 
 function App() {
   return (
-    // <Router>
     <Home />
-    // </Router>
   );
 }
 
--- src/components/WorkExperience/index.jsx
+++ src/components/WorkExperience/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { HashLink } from 'react-router-hash-link';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import './index.css';
 
