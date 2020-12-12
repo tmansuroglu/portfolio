2dda35fa78a765c6b67460106d09c4755c42a6a5
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-12-12 19:46:25 +0300

    put my image in better position

--- src/containers/About/index.jsx
+++ src/containers/About/index.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import './index.css';
-import { Container, Image } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Education from '../../components/Education';
 import EmploymentHistory from '../../components/WorkExperience';
 import TechnicalSkills from '../../components/TechnicalSkills';
 import WhoIsTarkan from '../../components/WhoIsTarkan';
-import tarkanImage from '../../img/tarkanImage.png';
 
 const About = () => {
   return (
     <Container id='about'>
       <h1 className='homeTitle aboutBigScreen'>About</h1>
       <div className='homeText'>
-        <div className='tarkanDiv'>
-          <Image
-            src={tarkanImage}
-            className='tarkanImage'
-            alt="tarkan mansuroğlu's picture"
-            roundedCircle
-            fluid
-          />
-        </div>
-        <h1 className='homeTitle aboutSmallScreen'>About</h1>
         <WhoIsTarkan />
         <Education />
         <EmploymentHistory />
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -6,6 +6,8 @@ import Introduction from '../../components/Introduction';
 import About from '../About';
 import Projects from '../Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import tarkanImage from '../../img/tarkanImage.png';
+import { Image, Container } from 'react-bootstrap';
 
 const Home = () => {
   return (
@@ -13,6 +15,15 @@ const Home = () => {
       <Navbar />
       <Sidebar />
       <Introduction />
+      <Container className='tarkanDiv'>
+        <Image
+          src={tarkanImage}
+          className='tarkanImage'
+          alt="tarkan mansuroğlu's picture"
+          roundedCircle
+          fluid
+        />
+      </Container>
       <About />
       <Projects />
     </>
