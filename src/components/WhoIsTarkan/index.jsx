9eea0dece3a831e7c0ab85b61953b2182287828d
Tarkan Mansuroğlu <mansuroglu99@gmail.com>
2021-08-24 01:44:36 +0300

    change text

--- src/components/WhoIsTarkan/index.jsx
+++ src/components/WhoIsTarkan/index.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const WhoIsTarkan = () => (
   <>
     <h3 id="whoIsTarkan">Who is Tarkan Mansuroğlu?</h3>
     <p>
-      Tarkan Mansuroğlu is a front-end web developer. He started learning coding
-      by himself and continued his coding education through “Re-Coded Front-End
-      Web Development Bootcamp”. He became a coder because he is a lifelong
-      learner and addicted to the sensation that comes after solving a tech
-      problem.
+      Tarkan Mansuroğlu is an ambitious and solution-driven front end developer
+      with a multi-disciplinary and multi-cultural background. Currently, Tarkan
+      is working for{' '}
+      <a href="https://smartup.network/" target="_blank" rel="noreferrer">
+        Smartup Network(SN)
+      </a>
+      . He is proficient at working in dynamic environments, teamwork, and
+      communication. Tarkan is experienced in JavaScript, HTML, CSS, React,
+      React Native, Vue, and Nuxt more. Check out{' '}
+      <AnchorLink offset="100" href="#technicalSkills">
+        technical skills
+      </AnchorLink>{' '}
+      to see what he knows.
+    </p>
+    <p>
+      Tarkan started learning coding by himself and continued his coding
+      education through “Re:Coded-Flatiron School Front-End Web Development
+      Bootcamp”. He became a coder because he is a lifelong learner and in love
+      with practicality of programming.
     </p>
     <p>
       He discovered his appetite for challenges in his early 20s. At age 20, he
       was severely obese and academically lost. At 22 he dropped from 175kg to
-      95 kg, became a fitness enthusiast and top finance student of his
+      95kg, became a fitness enthusiast and top finance student of his
       university. Later, he graduated with highest CGPA and continued being a
       physically active person.
     </p>
@@ -22,7 +37,7 @@ const WhoIsTarkan = () => (
       While searching for his purpose, he realized that to build his future,
       first, he needs to understand himself. This revelation pushed him to
       develop an analytical mindset. Consequently, he realized that problem
-      solving is the main drive of things he enjoys. Thus, when he started
+      solving is the main drive of the things he enjoys. Thus, when he started
       coding, he fell in love with it.
     </p>
   </>
--- src/components/WorkExperience/index.jsx
+++ src/components/WorkExperience/index.jsx
@@ -5,8 +5,21 @@ const EmploymentHistory = () => (
   <>
     <h3 id="employmentHistory">Work Experience</h3>
     <p>
-      Tarkan worked as junior developer intern for Re:Coded in 2020. His efforts
-      were focused on improving his{' '}
+      Tarkan is working for{' '}
+      <a href="https://smartup.network/" target="_blank" rel="noreferrer">
+        Smartup Network(SN)
+      </a>{' '}
+      as front-end developer since February 2021. Although his training was on
+      web development, Tarkan became a mobile developer while working for SN.
+      Checkout{' '}
+      <AnchorLink offset="100" href="#projects">
+        projects
+      </AnchorLink>{' '}
+      to see his professional efforts.
+    </p>
+    <p>
+      Before Smartup Network, Tarkan worked as junior developer intern for
+      Re:Coded in 2020. His efforts were focused on improving his{' '}
       <AnchorLink offset="100" href="#technicalSkills">
         technical skills
       </AnchorLink>
@@ -18,13 +31,11 @@ const EmploymentHistory = () => (
       management, time management...).
     </p>
     <p>
-      Before, he was peer teaching statistics, finance, economics and math to
-      social science students.
-    </p>
-    <p>
-      Also, he was working for banking and finance department of EMU as a
-      student assistant. His main responsibilities were: contributing to
-      department communication, archive management and translation.
+      During university, he was peer teaching statistics, finance, economics and
+      math to social science students. Also, he was working for banking and
+      finance department of EMU as a student assistant. His main
+      responsibilities were: contributing to department communication, archive
+      management and translation.
     </p>
   </>
 );
